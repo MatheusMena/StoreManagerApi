1151f72672b5aa57522a2e1fb255f680e96d3b42
MatheusMena <matheusmena1@hotmail.com>
2022-05-13 21:21:30 -0300

    fix(index.js): adicionando mensagem de erro e implementando busca por produtos

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const app = require('./app');
 require('dotenv').config();
+const storeModel = require('./models/storeModelProducts');
+
+app.get('/products', async (_req, res) => {
+const products = await storeModel.getAllProducts();
+
+res.status(200).json(products);
+});
+
+app.get('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const products = await storeModel.productsById(id);
+  if (!products) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+  return res.status(200).json(products);
+  });
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
